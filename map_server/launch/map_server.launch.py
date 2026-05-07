43a6950b2be640b5836792a67ee40eefc508f0d5
Jake Wright <wrightjake5683@gmail.com>
2026-05-07 02:07:16 +0000

    Finished creating map and map server

---
+++ map_server/launch/map_server.launch.py
@@ -0,0 +1,48 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    
+    map_file = os.path.join(get_package_share_directory('map_server'), 'config', 'warehouse_map_sim.yaml')
+    rviz_config = os.path.join(get_package_share_directory('map_server'), 'rviz', 'map_display.rviz')
+
+    return LaunchDescription([
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_transform_publisher_turtle_odom',
+            output='screen',
+            emulate_tty=True,
+            arguments=['0', '0', '0', '0', '0', '0', 'map', 'odom']
+        ),
+
+        Node(
+            package='nav2_map_server',
+            executable='map_server',
+            name='map_server',
+            output='screen',
+            parameters=[{'use_sim_time': True}, 
+                        {'yaml_filename': map_file} 
+            ]),
+
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_mapper',
+            output='screen',
+            parameters=[{'use_sim_time': True},
+                        {'autostart': True},
+                        {'node_names': ['map_server']}
+            ]),
+
+         Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            parameters=[{'use_sim_time': True}],
+            arguments = ['-d', rviz_config]
+        ),            
+    ])
